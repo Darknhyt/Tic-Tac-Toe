c5c832adad40a5c05ff764e28fa516065ddc17d8
Darknhyt <40185738+Darknhyt@users.noreply.github.com>
2023-11-21 17:50:17 -0400

    Adding score and info

--- script.js
+++ script.js
@@ -7,14 +7,18 @@ function Game(playerName1, playerName2) {
     let table = Table();
     let turn = 1;
     let winner = false;
-
-    //Public  get functions
+    table.newTable();
+    //Public functions
     const getTable = () => table.getTable();
     const getCurrentPlayer = () => current.getPlayerName();
+    const getCurrentPiece = () => current.getPlayerPiece();
+    const getPlayerName = (p) => p == 1 ? player1.getPlayerName() : player2.getPlayerName();
     const getWinner = () => winner;
+    const getPlayerPoints = (p) => players[p-1].getPoints(); 
+    const resetPoints = ()=>players.forEach((p)=>p.reset());
     console.log(table.printTable());
     console.log("Turn: " + getCurrentPlayer());
-   
+
     const play = (row, column) => { //Return an Object with the play Status
         let playStatus = {
             playerTurn: current.getPlayerName(),
@@ -30,21 +34,35 @@ function Game(playerName1, playerName2) {
                 console.log(table.printTable());
                 if (winCondition()) {
                     winner = current.getPlayerName();
+                    current.addPoint();
                     playStatus.playWinner = true;
                 } else {
                     turn++;
                     switchCurrentPlayer();
-                    if (turn == 10){
+                    if (turn == 10) {
                         console.log("DRAW")
                     } else {
                         console.log("Turn: " + getCurrentPlayer());
                     }
                 }
-            }      
-        } else { console.log("Game Finishied");}
+            }
+        } else { console.log("Game Finishied"); }
         return playStatus;
     }
-
+    const changeName = (num, newName) => {
+        if (num == 1) {
+            newName.length > 0 ?
+                player1.setPlayerName(newName) : player1setPlayereName("Player 1");
+        } else if (num == 2) {
+            newName.length > 0 ?
+                player2.setPlayerName(newName) : player2.setPlayerName("Player 2")
+        }
+    }
+    const newTable = () => {
+        table.newTable();
+        turn = 1;
+        winner = false;
+    }
     //Private functions
     function switchCurrentPlayer() {
         current = current == players[0] ?
@@ -52,7 +70,7 @@ function Game(playerName1, playerName2) {
     }
     function winCondition() {
         let win = false;
-        const pieceOn = (x, y)=> table.getBox(x, y) == current.getPlayerPiece();
+        const pieceOn = (x, y) => table.getBox(x, y) == current.getPlayerPiece();
 
         if (pieceOn(1, 1) && ((pieceOn(0, 0) && pieceOn(2, 2)) ||
             (pieceOn(0, 2) && pieceOn(2, 0)))) { //Diagonals
@@ -72,16 +90,24 @@ function Game(playerName1, playerName2) {
 
     //Player factory
     function Player(name, piece) {
+        this.name = name;
+        let points = 0;
         const getPlayerName = () => name;
         const getPlayerPiece = () => piece;
-        return { getPlayerName, getPlayerPiece }
+        const getPoints = () => points;
+        const setPlayerName = (newName) => name = newName;
+        const reset = ()=> points = 0;
+        const addPoint = ()=> points++;
+        return { getPlayerName, getPlayerPiece, setPlayerName, getPoints, reset, addPoint }
     }
 
     //Table factory
     function Table() {
         let box = [];
-        for (let b = 0; b < 3; b++) {
-            box.push([0, 0, 0]);
+        const newTable = () => {
+            for (let b = 0; b < 3; b++) {
+                box[b] = [0, 0, 0];
+            }
         }
         const getTable = () => box;
         const printTable = () => `${box[0]}\n${box[1]}\n${box[2]}`;
@@ -94,21 +120,82 @@ function Game(playerName1, playerName2) {
                 return false;
             }
         }
-        return { getTable, addPiece, printTable, getBox }
+        return { getTable, addPiece, printTable, getBox, newTable }
     }
-    return { getCurrentPlayer, getTable, play, getWinner }
+    return { getCurrentPlayer, getPlayerName, getCurrentPiece, getTable, play, getWinner, changeName, newTable, getPlayerPoints, resetPoints }
 }
-const game = Game("Player1", "Player2");
-init();
-function init(){
+function Controler() {
+    const game = Game("Player 1", "Player 2");
     const box = Array.from(document.getElementsByClassName("box"));
-    box.forEach((b)=>{
-        b.addEventListener("click",()=>{
-            const play = game.play(b.dataset.row,b.dataset.col);
-            if (play.pieceSuccess){
-                b.textContent = play.playerPiece == 1? "❌" : "⭕";
-            }
-        })
-    })
-}
+    const info = document.getElementById("info");
+    const getInput = (input) => {
+        return document.getElementById(input).value;
+    }
+    const setOutput = (output, info) => {
+        document.getElementById(output).textContent = info;
+    }
+    const setInfo = (newInfo) => {
+        info.classList.add("rotate");
+        setTimeout(()=>{
+            info.textContent = newInfo;
+        },100);
+        setTimeout(()=>{
+            info.classList.remove("rotate");
+        },200);
+    }
+    init();
 
+    function init() {
+        const newBtn = document.getElementById("opt-btn");
+        const rstBtn = document.getElementById("rst-btn");
+        box.forEach((b) => {
+            b.addEventListener("click", (e) => {
+                e.preventDefault();
+                const play = game.play(b.dataset.row, b.dataset.col);
+                if (play.pieceSuccess) {
+                    const [icon, nextIco] = getIco(play.playerPiece);
+                    b.textContent = icon;
+                    b.classList.add("busy");
+                    if (play.playWinner) {
+                        setInfo(`${icon} ${play.playerTurn} won!`);
+                        updateScore();
+                    } else if (play.turnOfGame < 9) {
+                        setInfo(`${nextIco} ${game.getCurrentPlayer()} turn`)
+                    } else {
+                        setInfo(`❌ DRAW ⭕`)
+                    }
+                }
+            })
+        })
+        newBtn.addEventListener("click", () => {
+            game.changeName(1, getInput("player-one"));
+            game.changeName(2, getInput("player-two"));
+            game.resetPoints();
+            setOutput("player-name-one", `${game.getPlayerName(1)}: `);
+            setOutput("player-name-two", `${game.getPlayerName(2)}: `);
+            setInfo(`${getIco(game.getCurrentPiece())[0]} ${game.getCurrentPlayer()} turn`);
+            resetTable();
+            updateScore();
+        })
+        rstBtn.addEventListener("click", () => {
+            resetTable();
+            setInfo(`${getIco(game.getCurrentPiece())[0]} ${game.getCurrentPlayer()} turn`);
+        })
+    }
+
+    function resetTable() {
+        game.newTable();
+        box.forEach((b) => {
+            b.textContent = "";
+            b.setAttribute("class", "box");
+        })
+    }
+    function getIco(playerPiece) {
+        return playerPiece == 1 ? ["❌", "⭕"] : ["⭕", "❌"]
+    }
+    function updateScore(){
+        setOutput("score-1", game.getPlayerPoints(1));
+        setOutput("score-2", game.getPlayerPoints(2));
+    }
+}
+const con = Controler()
